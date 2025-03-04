f86e4a9ebcdae87d3185633fc0fadbb424b3448e
silveira-vini <vsribeiro87@gmail.com>
2025-03-04 10:50:48 -0300

    config TS

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,6 @@
 import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 30, 1200);
+var negociacao = new Negociacao(new Date(), 30, 1200);
 console.log(negociacao.data);
 console.log(negociacao.volume);
+negociacao = new Negociacao(new Date(), 20, 30000);
+console.log(negociacao);
